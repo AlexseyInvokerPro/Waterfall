b091d2102f1702f7707cd09739db4e68d4765b65
Avdeychik <a.avdeychik@gmail.com>
2021-08-03 19:49:00 +0300

    First commit

---
+++ WaterFall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaterFall
+//
+//  Created by Алексей Авдейчик on 3.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
